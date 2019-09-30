003cb064092e7c2c5567a3641086d2f85c9caf7d
Victory <raccooccon@gmail.com>
2019-09-30 20:25:44 +0300

    Add ssolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = bracketsConfig.map(elem => elem.join(''));
+    while (arr.some(elem => str.includes(elem)) ) {
+      for (let i = 0; i < arr.length; i++) {
+        do {
+          str = str.replace(arr[i], '');
+        } while (str.indexOf(arr[i]) != -1);
+      }
+    }
+  return str.length == 0 ? true : false;
 }
